3b386a360c8d63d10eb79a39cac6b805eb97bcb3
PiotrCiosmak <piotrciosmak2001@gmail.com>
2022-05-09 17:47:32 +0200

    v0.8

---
+++ functions.hpp
@@ -0,0 +1,81 @@
+#include <cmath>
+#include <vector>
+
+constexpr int numberOfIterations{20};
+
+template<typename T>
+T function(T x)
+{
+    return sin(x);
+}
+
+template<typename T>
+T derivative(T x)
+{
+    return cos(x);
+}
+
+template<typename T>
+T fowardDifference2(T x, T h)
+{
+    return function(x + h) - function(x) / h;
+}
+
+template<typename T>
+T centralDifference2(T x, T h)
+{
+    return function(x + h) - function(x - h) / 2 * h;
+}
+
+template<typename T>
+T backwardDifference2(T x, T h)
+{
+    return function(x) - function(x - h) / h;
+}
+
+template<typename T>
+T fowardDifference3(T x, T h)
+{
+    return (-3 / 2.0 * function(x) + 2 * function(x + h) - 0.5 * function(x + 2 * h)) / h;
+}
+
+template<typename T>
+T backwardDifference3(T x, T h)
+{
+    return (3 / 2.0 * function(x) - 2 * function(x - h) + 0.5 * function(x - 2 * h)) / h;
+}
+
+template<typename T>
+void calculate(std::vector<std::vector<T>> &results, const T &begin, const T &end, T &h, const T &epsilon)
+{
+    T center = (begin - end) / 2;
+
+    for (int i = 0; i < numberOfIterations; ++i)
+    {
+        if (h < epsilon)
+            break;
+        results.push_back(std::vector<T>{0, 0, 0, 0, 0});
+        results.at(i).at(0) = fowardDifference2(center, h);
+        results.at(i).at(1) = centralDifference2(center, h);
+        results.at(i).at(2) = backwardDifference2(center, h);
+        results.at(i).at(3) = backwardDifference2(end, h);
+        results.at(i).at(4) = backwardDifference3(end, h);
+        h /= 1.05;
+    }
+}
+
+template<typename T>
+void difference(const std::vector<std::vector<T>> &results, std::vector<std::vector<T>> &differences, const T &begin, const T &end)
+{
+    T center = (begin - end) / 2;
+    for (int i = 0; i < results.size(); ++i)
+    {
+        differences.push_back(std::vector<T>{0, 0, 0, 0, 0});
+        differences.at(i).at(0) = fabs(results.at(i).at(0) - derivative(center));
+        differences.at(i).at(1) = fabs(results.at(i).at(1) - derivative(center));
+        differences.at(i).at(2) = fabs(results.at(i).at(2) - derivative(center));
+        differences.at(i).at(3) = fabs(results.at(i).at(3) - derivative(end));
+        differences.at(i).at(4) = fabs(results.at(i).at(4) - derivative(end));
+    }
+    //cout << endl << endl << "Order of accuracy" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include "functions.hpp"
+
+using namespace std;
+
+template<typename T>
+void saveToFile(const vector<vector<T>> &differences)
+{
+    fstream fowardDifference2Center("fowardDifference2Center.txt", ios::out);
+    fstream centralDifference2Center("centralDifference2Center.txt", ios::out);
+    fstream backwardDifference2Center("backwardDifference2Center.txt", ios::out);
+    fstream backwardDifference2End("backwardDifference2End.txt", ios::out);
+    fstream backwardDifference3End("backwardDifference3End.txt", ios::out);
+
+    for (int i = 0; i < differences.size(); ++i)
+    {
+        fowardDifference2Center << log10(differences.at(i).at(0)) << ";";
+        centralDifference2Center << log10(differences.at(i).at(1)) << ";";
+        backwardDifference2Center << log10(differences.at(i).at(2)) << ";";
+        backwardDifference2End << log10(differences.at(i).at(3)) << ";";
+        backwardDifference3End << log10(differences.at(i).at(4)) << ";";
+    }
+
+    fowardDifference2Center.close();
+    centralDifference2Center.close();
+    backwardDifference2Center.close();
+    backwardDifference2End.close();
+    backwardDifference3End.close();
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector<vector<double>> results;
+    vector<vector<double>> differences;
+    constexpr double begin{0};
+    constexpr double end{M_PI / 2};
+    double h{1};
+    constexpr double epsilon{1e-10};
+    calculate(results, begin, end, h, epsilon);
+    difference(results, differences, begin, end);
+    saveToFile(differences);
 }
